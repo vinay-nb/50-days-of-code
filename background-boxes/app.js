f106665feb3f05cb8f9fec66f58c883150fe127e
Vinay Bhat <vinaybhathg11@gmail.com>
2023-06-04 10:13:49 +0530

    bakcground-boxes

---
+++ background-boxes/app.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("btn");
+const boxContainer = document.getElementById("boxes");
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxContainer.appendChild(box);
+    }
+  }
+}
+
+btn.addEventListener("click", () => {
+  boxContainer.classList.toggle("big");
+});
+
+createBoxes();
